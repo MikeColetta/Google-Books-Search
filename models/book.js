6f3e56a723600e97c5dff8590143eb5bc2cf009f
Mike Coletta <coletta.mike@gmail.com>
2021-05-17 11:36:22 -0700

    Added _id to book

--- models/book.js
+++ models/book.js
@@ -2,8 +2,9 @@ const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 
 const bookSchema = new Schema({
-    authors: [{ type: String, required: true }],
-    description: [{ type: String, required: true }],
+    _id: { type: String },
+    authors: { type: Array, required: true },
+    description: { type: String, required: true },
     image: { type: String, required: true },
     link: { type: String, required: true },
     title: { type: String, required: true },
