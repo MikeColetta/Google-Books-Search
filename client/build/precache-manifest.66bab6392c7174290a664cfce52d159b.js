c5e5d3c24355a26853f4aa69dd1e1752c573fe72
Mike Coletta <coletta.mike@gmail.com>
2021-05-17 21:10:31 -0700

    Created build

---
+++ client/build/precache-manifest.66bab6392c7174290a664cfce52d159b.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ca36067caa909973b86a2e4d31c67904",
+    "url": "/index.html"
+  },
+  {
+    "revision": "7333a7a99a3523c54dff",
+    "url": "/static/css/main.4d041d89.chunk.css"
+  },
+  {
+    "revision": "c6933b461da03d745030",
+    "url": "/static/js/2.1babd22c.chunk.js"
+  },
+  {
+    "revision": "9b318b6fb13190fe82c0677e9264b3c7",
+    "url": "/static/js/2.1babd22c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7333a7a99a3523c54dff",
+    "url": "/static/js/main.bb442348.chunk.js"
+  },
+  {
+    "revision": "d08766e1dd2bf4f6c209",
+    "url": "/static/js/runtime-main.992eac29.js"
+  },
+  {
+    "revision": "32e45b5394c50179fe0da36a961a25a5",
+    "url": "/static/media/library.32e45b53.jpg"
+  }
+]);
