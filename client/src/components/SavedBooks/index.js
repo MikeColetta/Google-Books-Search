b32186ba223f852aa5554d8d00715a21a4339bd3
Mike Coletta <coletta.mike@gmail.com>
2021-05-17 17:05:03 -0700

    Added delete saved books functionality

--- client/src/components/SavedBooks/index.js
+++ client/src/components/SavedBooks/index.js
@@ -1,20 +1,34 @@
 import React from "react";
 
-function SavedBooks(props) {
+
+
+function SavedBooks({books, deleteBook}) {
     return (
         <div className="container card">
-            <ul class="list-group">
-                <div className="card">
-                    <li class="list-group-item">Books go here!</li>
+                    {books.length ? (books.map((result, index) => (
+                        <div className="card mb-3" key={result._id}>
+                            <div className="row">
+                                <div className="col-md-2">
+                                    <img alt={result.title} className="img-fluid" src={result.image} />
+                                </div>
+                                <div className="col-md-10">
+                                    <div className="card-body">
+                                        <div className="row">
+                                            <h5 className="card-title col-8">{result.title} by {result.authors}</h5>
+                                            <a href={result.link} className="btn btn-primary btn-outline-dark col-2" target="_blank" rel="noopener noreferrer" >More</a>
+                                            <button data-index={result._id} onClick={deleteBook} className="btn btn-danger btn-outline-dark col-2 ml-2 customBookButton" target="_blank" rel="noopener noreferrer" >
+                                                Delete
+                                            </button>
+                                        </div>
+                                        <div className="row">
+                                            <p className="card-text">{result.description}</p>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    ))) : (<h2 style={{ textAlign: "center" }}>Search for a book!</h2>)}
                 </div>
-                <div className="card">
-                    <li class="list-group-item">Books go here!</li>
-                </div>
-                <div className="card">
-                    <li class="list-group-item">Books go here!</li>
-                </div>
-            </ul>
-        </div>
     )
 }
 
--- client/src/pages/Saved.js
+++ client/src/pages/Saved.js
@@ -1,13 +1,46 @@
 import React from 'react';
 import Jumbotron from '../components/Jumbotron';
 import SavedBooks from '../components/SavedBooks';
+import API from '../utils/API';
+
 
 class Saved extends React.Component {
+    
+    state = {
+        savedBooks: []
+    }
+
+    componentDidMount(){
+        this.getSavedBooks();
+    }
+
+    deleteBook = event =>{
+        console.log('delete')
+        let id = event.target.getAttribute("data-index")
+        API.deleteBook(id)
+            .then(this.setState(savedBooks))
+            .catch(err => console.error(err));
+    };
+
+    getSavedBooks() {
+        API.getBooks()
+            .then(res => {
+                console.log(res)
+                this.setState({ savedBooks: res.data })
+            })
+            .catch(err => console.error(err));
+    };
+
     render() {
+        console.log(this.state.savedBooks)
     return (
         <div>
             <Jumbotron />
-            <SavedBooks />
+            <SavedBooks 
+                books = {this.state.savedBooks}
+                deleteBook = {this.deleteBook}
+
+            />
         </div>
     )
     }
