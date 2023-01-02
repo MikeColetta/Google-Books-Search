1256c565849c7b30c0a165b425e347403f88a90d
Mike Coletta <coletta.mike@gmail.com>
2023-01-02 02:17:20 -0800

    Removed global state

--- client/src/utils/API.js
+++ client/src/utils/API.js
@@ -2,23 +2,23 @@ import axios from 'axios';
 
 export default {
     //Grabs a book from the Google API
-    getGooglebooks: function(query) {
+    getGooglebooks: function (query) {
         return axios.get(`https://www.googleapis.com/books/v1/volumes?q=${query}`)
     },
     //Saves a book to DB
-    saveBook: function(bookInfo) {
+    saveBook: function (bookInfo) {
         return axios.post("/api/books", bookInfo);
     },
     //Gets a book from the saved books DB
-    getBooks: function() {
+    getBooks: function () {
         return axios.get("/api/books");
     },
     //Gets a book by its ID
-    getBook: function(id) {
+    getBook: function (id) {
         return axios.get("/api/books/" + id)
     },
     //Deletes a book from the DB
-    deleteBook: function(id) {
+    deleteBook: function (id) {
         return axios.delete("/api/books/" + id);
     }
 }
